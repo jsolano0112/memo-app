7107b06bcf69749f16318f8fc340634c3a1a8bf1
Juana Solano <juana.solano@correo.tdea.edu.co>
2023-11-01 18:30:17 -0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>Hello</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
